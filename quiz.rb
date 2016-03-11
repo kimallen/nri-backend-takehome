531599752afebd4d51c766d717c3184424a07d16
kimallen <kimmarieallen@gmail.com>
2016-03-11 00:31:13 -0800

    Return list for one strand

--- quiz.rb
+++ quiz.rb
@@ -35,6 +35,7 @@ end
 class Standard
 	attr_accessor :standard_id, :standard_name
 	attr_reader :questions
+	
 	def initialize(args = {})
 	@standard_id = args[:standard_id]
 	@standard_name = args[:standard_name]
@@ -51,17 +52,26 @@ class Question
 end
 
 class CustomQuiz
-	attr_accessor :counter_hash
+	attr_accessor :counter_hash, :standards_counts, :questions_counts, :question_ids
 	attr_reader :num_questions
+
 	def initialize(num_questions, strands)
 		@num_questions = num_questions
 		@strands = strands
 		@standards_counts = {}
-		@question_counts = {}
+		@questions_counts = {}
 		@quiz_questions = []
+		@question_ids = []
 	end
 
 	#creating a method to create a list of strands to cycle through
+	def question_ids_list
+		self.questions_for_quiz.each do |q|
+			@question_ids << q.question_id
+		end
+		p @question_ids[0..(@num_questions-1)]
+	end
+
 	def questions_for_quiz
 		# until the length of @quiz_questions is >= num_questions
 			# in each strand alternately
@@ -69,48 +79,62 @@ class CustomQuiz
 					# find the question with the minumum count
 						# add that question to the list
 		# return the array of question_list of length of num_questions
-		until @quiz_questions.length >= @num_questions
+		self.standards_starting_count
+		self.questions_starting_count
+		# until @quiz_questions.length >= @num_questions
 			
-			min_key = strands[0].standards.min_by |k, v| {v}.key
-
-			strands[0].standards.each do |standard|
-				if standard.standard_name == min_key
-					standards_counter(min_key)
+			@strands[0].standards.each do |standard|
+				min_used_standard = @standards_counts.min_by {|k, v| v}
+				if standard.standard_name == min_used_standard[0]
+					standards_counter(min_used_standard[0])
 				end
+				#returns all that meet min (as array key/value pairs)
+				min_used_question = questions_counts.min_by {|k, v| v}
 				
-				min_key = standard.questions.min_by |k, v|{v}.key
 				standard.questions.each do |question|
-					if question.question_id == min_key
+					if question.question_id == min_used_question[0]
+						questions_counter(min_used_question[0])
 						@quiz_questions << question
 					end
 				end
+				if @quiz_questions.length >= @num_questions
+					p @standards_counts
+					p @questions_counts
+					return @quiz_questions
+				end
 			end
-		end
-		# @strands[1].standards[0].questions[0]
+		# end
 	end
 	#creates hash of all standards with value (num used in quiz) set to 0
 	def standards_starting_count
 		@strands.each do |strand|
 			strand.standards.each do |standard|
-			@standards_counts[standard.standard_name] = 0
+				@standards_counts[standard.standard_name] = 0
+			end
 		end
 		@standards_counts
 	end
+
 	#Adds to each item in the standard_counts as it is utilized in the quiz
 	def standards_counter(category)
 		@standards_counts[category] += 1
-	end
-
-	def questions_starting_count
-			@standards.each do |standard|
-			standard.questions.each do |question|
-			@questions_counts[question.question_id] = 0
-		end
 		@standards_counts
 	end
 
-	def standards_counter(category)
+	def questions_starting_count
+			@strands.each do |strand|
+				strand.standards.each do |standard|
+					standard.questions.each do |question|
+						@questions_counts[question.question_id] = 0
+					end
+			end
+		end
+		@questions_counts
+	end
+
+	def questions_counter(category)
 		@questions_counts[category] += 1
+		@questions_counts
 	end
 
 end
@@ -172,5 +196,7 @@ parser = Parser.new("questions.csv")
 strands = parser.parse_file
 strands[0].standards[0].questions[0]
 quiz = CustomQuiz.new(5, parser.strands)
-p quiz.questions_for_quiz
+quiz.questions_for_quiz
+p quiz.question_ids_list
+
 	
