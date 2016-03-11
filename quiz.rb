fca5a476738ca3854acf3df4f08615edfee75b4e
kimallen <kimmarieallen@gmail.com>
2016-03-10 22:56:53 -0800

    Create method for creating quiz list(not tested)

--- quiz.rb
+++ quiz.rb
@@ -32,7 +32,6 @@ class Strand
 		@strand_name = args[:strand_name]
 		@standards = []
 	end
-
 end
 
 class Standard
@@ -54,26 +53,67 @@ class Question
 end
 
 class CustomQuiz
+	attr_accessor :counter_hash
 	attr_reader :num_questions
-	def initialize(num_questions, file_questions)
+	def initialize(num_questions, strands)
 		@num_questions = num_questions
-		@file_questions = file_questions
+		@strands = strands
+		@standards_counts = {}
+		@question_counts = {}
+		@quiz_questions = []
 	end
 
 	#creating a method to create a list of strands to cycle through
 	def questions_for_quiz
-		quiz_questions = []
-		all_strands = [1..@file_questions.length]
-		all_standards = [1..6]
-		all_questions = [1..12]
-		
-		@file_questions[1].standards[0].questions[0]
+		# until the length of @quiz_questions is >= num_questions
+			# in each strand alternately
+				# find the standard with the minimum count
+					# find the question with the minumum count
+						# add that question to the list
+		# return the array of question_list of length of num_questions
+		until @quiz_questions.length >= @num_questions
+			
+			min_key = strands[0].standards.min_by |k, v| {v}.key
 
-	
+			strands[0].standards.each do |standard|
+				if standard.standard_name == min_key
+					standards_counter(min_key)
+				end
+					min_key = standard.questions.min_by |k, v|{v}.key
+					standard.questions.each do |question|
+						if question.question_id == min_key
+							@quiz_questions << question
+						end
+					end
+				end
+		end
+		# @strands[1].standards[0].questions[0]
 	end
-	
-	def generate_quiz(num_questions)
+	#creates hash of all standards with value (num used in quiz) set to 0
+	def standards_starting_count
+		@strands.each do |strand|
+			strand.standards.each do |standard|
+			@standards_counts[standard.standard_name] = 0
+		end
+		@standards_counts
 	end
+	#Adds to each item in the standard_counts as it is utilized in the quiz
+	def standards_counter(category)
+		@standards_counts[category] += 1
+	end
+
+	def questions_starting_count
+			@standards.each do |standard|
+			standard.questions.each do |question|
+			@questions_counts[question.question_id] = 0
+		end
+		@standards_counts
+	end
+
+	def standards_counter(category)
+		@questions_counts[category] += 1
+	end
+
 end
 
 class Parser
@@ -86,6 +126,7 @@ class Parser
 		@strands = []
 	end
 
+	# returns an object of strands (@strands) that have standards that have questions
 	def parse_file
 
 		CSV.foreach(@file, :headers => true) do |row|
